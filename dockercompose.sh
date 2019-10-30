0243c39e1cef9ed4c243978c776d5c8c7312507c
harryma <machengxue707@gmail.com>
2019-10-30 15:29:51 +0800

    hello

---
+++ dockercompose.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x 
+sudo yum install epel-release
+
+sudo yum install -y python-pip
+
+
+sudo pip install docker-compose
+
+
+#https://github.com/NaturalHistoryMuseum/scratchpads2/wiki/Install-Docker-and-Docker-Compose-(Centos-7)
