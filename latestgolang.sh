aec6d2da5f5d6c2cf7df899b4e617b38c4b48c55
harry <machengxue707@gmail.com>
2019-11-08 14:36:36 +0800

    add golang script

--- latestgolang.sh
+++ latestgolang.sh
@@ -22,7 +22,7 @@ if [[ $? -ne 0 ]];then
 fi
 
 #下载某一个版本的压缩包，解压，放到/usr/local中
-$ManagerCommand update  && wget https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz && tar -xzf go1.13.3.linux-amd64.tar.gz && mv go /usr/local
+$ManagerCommand update  && wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz && tar -xzf go1.13.4.linux-amd64.tar.gz && mv go /usr/local
 
 
 
